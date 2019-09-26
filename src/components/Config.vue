e125f89e3f13271fc9d2998091ddb0c3ad044b07
renzomondragon <renzodesign7@gmail.com>
2019-09-26 03:54:04 -0500

    visitas, perfil de usuario

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -1,20 +1,104 @@
 <template>
     <div>
-        <h5>configuracion</h5>
-    </div>
+      <!-- <div class="flex-grow-1"></div> -->
+      <!-- <v-dialog v-model="dialog" max-width="1000px" class="center"> -->
+<!--       <template v-slot:activator="{ on }">
+          <v-btn color="#ff4200" style="color: #FAFAFA;" dark class="mb-2" v-on="on">Editar Perfil</v-btn>
+       </template> -->
+         <v-card> 
+              <v-container>
+                <v-row>
+                    <v-col cols="1" md="3" sm="10">
+                        <v-avatar size="100">
+                            <img
+                                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                                alt="John"
+                            >
+                            </v-avatar>
+                        </v-col>
+                  <v-col cols="20" sm="3" md="80" class=center>
+                    <v-text-field
+                            prepend-icon="account_circle"
+                            ref="fullname"
+                            v-model="fullname"
+                            label="Full Name"
+                            placeholder=""
+                            disabled=""
+                            
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="20" sm="3" md="80" class=center>
+                      <v-text-field
+                            prepend-icon="email"
+                            ref="email"
+                            v-model="email"
+                            label="E-mail"
+                            placeholder=""
+                            disabled=""
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="20" sm="3" md="80" class=center>
+                      <v-text-field
+                            prepend-icon="account_circle"
+                            ref="role"
+                            v-model="role"
+                            label="Role"
+                            placeholder=""
+                            disabled=""
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="20" sm="3" md="80" class=center>
+                      <v-text-field
+                            prepend-icon="group"
+                            ref="groupsegment"
+                            v-model="groupsegment"
+                            label="Grupo de Segmento"
+                            disabled=""
+                            placeholder=""
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="20" sm="3" md="80" class=center>
+                      <v-text-field
+                            prepend-icon="account_circle"
+                            ref="jefe"
+                            v-model="jefe"
+                            label="Jefe"
+                            disabled=""
+                            placeholder=""
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+              </v-container>
+            <v-card-actions>
+                      <div class="flex-grow-1"></div>
+                      <v-btn color="#ff4200" text @click="close">Cancel</v-btn>
+                      <v-btn color="#ff4200" text @click="save()">Save</v-btn>
+         </v-card-actions>
+        </v-card>
+  <!--   </v-dialog> -->
+</div>
 </template>
+
+
 <script>
 export default {
-    data() {
+    data: () => ({
+      fullname: null,
+      email: null,
+      role: null,
+      groupsegment: null,
+      jefe: null,
+    }),
+    computed: {
+      form () {
         return {
-            
+          fullname: this.fullname,
+          email: this.email,
+          role: this.role,
+          groupsegment: this.groupsegment,
+          jefe: this.jefe,
         }
-    },
-    mounted() {
-       
-    },
-    updated() {
-      
+      },
     },
 }
 </script>
--- src/components/Visits.vue
+++ src/components/Visits.vue
@@ -27,9 +27,74 @@
     </v-data-table>
 
 </v-container>
-<v-col cols="auto" style="margin-top: 10px;">
-        <v-btn color="#ff4200" >Registrar Visita</v-btn>
-</v-col>
+<template>
+    <div>
+      <v-spacer></v-spacer>
+      <div class="flex-grow-1"></div>
+      <v-dialog v-model="dialog" max-width="1000px" class="center">
+      <template v-slot:activator="{ on }">
+          <v-btn color="#ff4200" style="color: #FAFAFA;" dark class="mb-2" v-on="on">Registrar Nueva Visita</v-btn>
+       </template>
+         <v-card>
+            <v-toolbar-title>Gestión de Visitas</v-toolbar-title>
+              <v-container>
+                <v-row>
+                  <v-col cols="20" sm="3" md="80" class=center>
+                    <v-combobox
+                      v-model="select"
+                      :items="items"
+                      label="Seleccionar Motivo"
+                    ></v-combobox>
+                  </v-col>
+                  <v-col cols="20" sm="3" md="80" class=center>
+                    <v-combobox
+                      v-model="select"
+                      :items="items"
+                      label="Seleccionar Empresa"
+                    ></v-combobox>
+                  </v-col>
+                  <v-col cols="20" sm="3" md="80" class=center>
+                      <v-menu
+                        ref="menu"
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :return-value.sync="date"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on }">
+                          <v-text-field
+                            v-model="date"
+                            label="Fecha de Visita"
+                            prepend-icon="event"
+                            readonly
+                            v-on="on"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="date" no-title scrollable>
+                          <div class="flex-grow-1"></div>
+                          <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+                          <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                        </v-date-picker>
+                      </v-menu>
+                    </v-col>
+                    <!-- <v-col cols="20" sm="3" md="80" class=center>
+                      <v-switch v-model="switch1" inset :label="`Status: ${switch1.toString()}`"></v-switch> 
+                    </v-col> -->
+                </v-row>
+              </v-container>
+
+            <v-card-actions>
+                      <div class="flex-grow-1"></div>
+                      <v-btn color="#ff4200" text @click="close">Cancel</v-btn>
+                      <v-btn color="#ff4200" text @click="save()">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+          </v-dialog>
+    </div>
+    </template>
+
     <div>
         <template>
           <v-row>
@@ -84,6 +149,9 @@ import axios from "axios";
 import { mapState, mapActions } from 'vuex';
 export default {
   data: () => ({
+    search: "",
+    switch1: true,
+    switch2: false,
     state: true,
     today: '2019-01-08',
     events: [
@@ -186,7 +254,9 @@ export default {
       }
       
       
-    } 
+    },
+    date: new Date().toISOString().substr(0, 10),
+    menu: false,
   
 };
 </script>
